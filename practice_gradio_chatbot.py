d3fd5b5161488a050183fe42f3dd96015c0f5c7a
Minh Pham <67337723+minhp03@users.noreply.github.com>
2025-01-06 19:41:50 -0500

    Add files via upload
    Controversial chatbot application using Gradio and mini-gpt-4o
    
    I made a chatbot calling API from chatgpt with gradio,openai
    
    FEATURE:
     + chatbot will memorize the conversation with stream answer.

---
+++ practice_gradio_chatbot.py
@@ -0,0 +1,50 @@
+
+
+
+
+import gradio as gr
+import random
+from openai import OpenAI
+
+client = OpenAI(
+   
+    api_key = "your-api",
+)
+
+def chat_logic(message,chat_history):
+    messages = []
+    for msg_pair in chat_history:
+        messages.append({"role":"user","content":msg_pair[0]})
+        messages.append({"role":"assistant","content":msg_pair[1]})
+    #link question of user and answer of bot.
+    messages.append({"role":"user","content":message})
+    #print(messages)
+
+    chat_completion = client.chat.completions.create(
+      model="gpt-4o-mini",
+      messages=messages,
+      stream=True)
+    chat_history.append([message,"loading..."])
+
+    yield "", chat_history
+
+    chat_history[-1][1]= ""
+    for chunk in chat_completion:
+       delta = chunk.choices[0].delta.content or ""
+       chat_history[-1][1] += delta
+       yield "", chat_history
+
+    return "",chat_history
+
+
+
+
+with gr.Blocks() as demo:
+  gr.Markdown("# CHAT BOT WITH CHATGPT")
+  message = gr.Textbox(label="input message")
+  chatbot = gr.Chatbot(label="Superduper chatbot")
+  message.submit(chat_logic,[message,chatbot],[message,chatbot])
+
+demo.launch(debug=True)
+#gr.Error("custom message")
+
